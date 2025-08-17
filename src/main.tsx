4f7dbf64673d45cad724e371333caa4339d8b1de
Ramy Nady <andmohamedramy@gmail.com>
2025-08-17 23:39:50 +0500

    Финальные исправления для корректной публикации на GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { StoreProvider } from "./store/StoreProvider";
 import "./styles/index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <StoreProvider>
-      <App />
+    <BrowserRouter basename="/MobX-Task-Tree-App">
+       <App />
+    </BrowserRouter>
     </StoreProvider>
   </React.StrictMode>
 );
