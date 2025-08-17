53ba4897579a390b64b33541248157c7cd82115e
Ramy Nady <andmohamedramy@gmail.com>
2025-08-18 01:01:56 +0500

    Убраны роутеры для одностраничного приложения

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { StoreProvider } from "./store/StoreProvider";
-import {  HashRouter } from "react-router-dom";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
       <StoreProvider>
         <App />
       </StoreProvider>
-    </HashRouter>
   </React.StrictMode>
 );
